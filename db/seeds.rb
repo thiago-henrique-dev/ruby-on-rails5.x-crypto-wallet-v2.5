0c07a25bb754d1d0f6c86544b643d10025edd1b6
thiagohenriquedev <thiagohenriquedev>
2022-10-17 18:47:19 -0500

    A task db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cadastrando moedas..."
+
+Coin.create!(
+    description: "Bitcoin",
+    acronym: "BTC",
+    url_image: "https://imagensemoldes.com.br/wp-content/uploads/2020/09/Logo-Bitcoin-PNG.png",
+
+)
+
+
+Coin.create!(
+    description: "Etherum",
+    acronym: "ETH",
+    url_image: "https://upload.wikimedia.org/wikipedia/commons/b/b7/ETHEREUM-YOUTUBE-PROFILE-PIC.png",
+    
+)
+
+
+Coin.create!(
+    description: "Dash",
+    acronym: "DASH",
+    url_image: "https://w7.pngwing.com/pngs/853/418/png-transparent-logo-dash-cryptocurrency-ethereum-steemit-ripple-coin-blue-text-trademark.png",
+    
+)
+
+puts "Moedas cadastradas com sucesso!"
