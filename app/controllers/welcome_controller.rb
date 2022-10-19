e1180c7e30cc156113f3cc7997ee1183d0bbcc50
thiagohenriquedev <thiagohenriquedev>
2022-10-19 09:25:05 -0300

    sessions and cookies

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby On Rails [COOKIE]"
+    session[:user_name] = "Thiago"
     @meu_nome = params[:nome]
     @curso = params[:curso]
   end
